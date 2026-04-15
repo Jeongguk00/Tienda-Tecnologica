92abcf748188bed40bf6cd8f96edbee6393e92a9
stiven sana <stiven.sana.dev@gmail.com>
2026-04-14 22:08:58 -0500

    feat: menu terminado con reportes

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const productos = [
 
     {id: 1, nombre: "Mouse", Categoria: "Periferico", 
-        Precio: 50000, stock: 10, ventas: 12},
+        precio: 50000, stock: 10, ventas: 12},
 
         {id: 2, nombre: "Teclado", Categoria: "Periferico", 
-        Precio: 120000, stock: 5, ventas: 7},
+        precio: 120000, stock: 5, ventas: 7},
 
         {id: 3, nombre: "Monitor", Categoria: "Pantalla", 
-        Precio: 800000, stock: 2, ventas: 4},
+        precio: 800000, stock: 2, ventas: 4},
 
         {id: 4, nombre: "USB", Categoria: "Accesorio", 
-        Precio: 30000, stock: 0, ventas: 15},
+        precio: 30000, stock: 0, ventas: 15},
 
         {id: 5, nombre: "Parlante", Categoria: "Audio", 
-        Precio: 90000, stock: 8, ventas: 6},
+        precio: 90000, stock: 8, ventas: 6},
 
 
 ];
+
+const reportesDiv = document.getElementById("reportes");
+
 
 const contenedor = 
 document.getElementById("productos"); 
@@ -45,25 +48,51 @@ function mostrarProductos() {
 
 mostrarProductos();
 
-
-
 function mostrarStockBajo() {
-    const stockBajo = productos.filter(p => p.stock > 0 && p.stock < 5);
+  const stockBajo = productos.filter(p => p.stock > 0 && p.stock < 5);
 
-    console.log("productos con stock bajo: ");
-        console.log(stockBajo);
-
-        return stockBajo;
+  contenedor.innerHTML = stockBajo.map(p => `
+    <div class="producto">
+      <h3>${p.nombre}</h3>
+      <p>⚠ Stock bajo: ${p.stock}</p>
+    </div>
+  `).join("");
 }
 
 function mostrarAgotados() {
-    const agotados = productos.filter(p => p.stock === 0);
+  const agotados = productos.filter(p => p.stock === 0);
 
-     console.log("productos agotados: ");
-     console.log(agotados);
-
-     return agotados;
+  contenedor.innerHTML = agotados.map(p => `
+    <div class="producto">
+      <h3>${p.nombre}</h3>
+      <p>✖️GOTADO</p>
+    </div>
+  `).join("");
 }
 
-mostrarStockBajo();
-mostrarAgotados();
+function generarReportes() {
+
+  const masCaro = [...productos].sort((a,b)=>b.precio-a.precio)[0];
+  const masBarato = [...productos].sort((a,b)=>a.precio-b.precio)[0];
+  const masVendido = [...productos].sort((a,b)=>b.ventas-a.ventas)[0];
+
+  const valorInventario = productos.reduce((total, p) => {
+    return total + (p.precio * p.stock);
+  }, 0);
+
+  const totalVentas = productos.reduce((total, p) => {
+    return total + p.ventas;
+  },0);
+
+
+  reportesDiv.innerHTML = `
+
+    <h2>📊 Reportes de la tienda</h2>
+    <p>💰 Más caro: ${masCaro.nombre}</p>
+    <p>💸 Más barato: ${masBarato.nombre}</p>
+    <p>🏆 Más vendido: ${masVendido.nombre}</p>
+    <p>📦 Total ventas: ${totalVentas}</p>
+    <p>💵 Valor inventario: $${valorInventario}</p>
+    
+  `;
+}
